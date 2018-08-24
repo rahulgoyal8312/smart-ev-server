eb7f4b227ef37164a11fd045186d87eac5a94e0c
Rahul Goyal <rahulgoyal8312@gmail.com>
2018-08-24 23:43:12 +0530

    nearest & search station added

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -64,4 +64,61 @@ class StationController extends Controller
 			}	
 		}
     }
+
+
+    public function getNearestStations(Request $request){
+        $validator = Validator::make($request->all(),[
+            'radius'=> 'required',
+            'latitude'=>'required','regex:^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$',
+            'longitude'=>'required','regex:^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$',
+        ]);
+        $response = new ApiResponse();
+        if ($validator->fails()) {
+            $arr = $validator->errors()->getMessages();
+            $keys = array_keys($arr);
+            return $response->showError($arr[$keys[0]][0]);
+        }
+        else{
+            try{
+                $lat = request('latitude');
+                $lng = request('longitude');
+                $radius = request('radius');
+                $station = DB::table('stations')
+                ->select('id','name','address','latitude','longitude',DB::raw('('.$radius.' * acos (cos ( radians('.$lat.') )* cos(radians( latitude ) )* cos( radians( longitude ) - radians('.$lng.') )+ sin ( radians('.$lat.') )* sin( radians( latitude ) ) )) as distance'))
+                ->having('distance','<','20')
+                ->orderBy('distance')
+                ->take('10')
+                ->get();
+                return $response->showData($station);
+            }
+            catch(Exception $e){
+            $error_log = new ErrorLog();
+            $error_log->message = $e->getMessage();
+            $error_log->save();
+            return $response->showError("Data access error : " . $e->getMessage());
+            }
+
+        }
+    }
+
+    public function searchStation($name,Request $request){
+        $response = new ApiResponse();
+        try{
+            $station = Station::select('*')
+                    ->where('name','LIKE',$name.'%')
+                    ->get();
+            if(count($station)>0){
+            return $response->showData($station);
+            }
+            else{
+                return $response->showError('Details not found');
+            }        
+        }
+        catch(Exception $e){
+        $error_log = new ErrorLog();
+        $error_log->message = $e->getMessage();
+        $error_log->save();
+        return $response->showError("Data access error : " . $e->getMessage());   
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/get_user/{user_id}', 'UserController@getUser');
 Route::post('/add_user', 'UserController@addUser');
 Route::post('/add_station','StationController@addStation');
 Route::get('/get_station/{id}', 'StationController@getStation');
-
+Route::post('/get_nearest_stations','StationController@getNearestStations');
+Route::get('/search_station/{name}','StationController@searchStation');
